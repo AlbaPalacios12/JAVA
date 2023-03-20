65fd1b4e88e99e977ad3b7d8966e2ee844a850f7
AlbaPalacios12 <126180027+AlbaPalacios12@users.noreply.github.com>
2023-03-20 10:58:52 +0100

    Add files via upload

---
+++ aresuelta72/CuentaCorriente.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aresuelta72;
+
+/**
+ *
+ * @author 34625
+ */
+class CuentaCorriente {
+
+    String dni;
+    String nombre;
+    double saldo;
+
+    CuentaCorriente(String dni, String nombre) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.saldo = 0;
+    }
+
+    boolean sacar(double cant) {
+        boolean operacionPosible;
+        if (saldo >= cant) {
+            saldo -= cant;
+            operacionPosible = true;
+        } else {
+            operacionPosible = false;
+            System.out.println("No hay dinero suficiente");
+
+        }
+        return (operacionPosible);
+    }
+
+    void ingreso(double cant) {
+        saldo += cant;
+    }
+
+    void mostrarInformacion() {
+        System.out.println("Nombre " + nombre);
+        System.out.println("DNI " + dni);
+        System.out.println("Saldo " + saldo + " euros");
+    }
+
+    CuentaCorriente(String dni, double saldo) {
+this.dni=dni;
+this.saldo=saldo;
+this.nombre= "Sin asignar";
+    }
+
+    public CuentaCorriente(String dni, String nombre, double saldo) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.saldo = saldo;
+    }
+}
