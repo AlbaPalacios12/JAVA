39e31f6b7533c954639aebe80cfb665b97e42aef
AlbaPalacios12 <126180027+AlbaPalacios12@users.noreply.github.com>
2023-03-22 19:32:14 +0100

    Add files via upload

---
+++ AResuelta712/public class Main {.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Lista11 = new Lista ();
+        Lista12 = new Lista ();
+        11.insertarFinal(1);  11.insertarFinal (1) ; 11.insertarFinal (2); 
+        11.insertarFinal(3);
+        12.insertarFinal(10); 12.insertarFinal (20) ; 12.insertarFinal (30);
+        Lista concatenacion= Lista.concatenar(Lista11,Lista12);
+        concatenacion.mostrar();
+    }
+}
